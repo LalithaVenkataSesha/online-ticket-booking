223d15964a2b2ca7b14055e36c4c3f7a883bd111
Lalitha pelluri <maruthilalitha02@gmail.com>
2020-12-08 20:10:51 -0800

    my first commit

---
+++ src/app/search-busses/search-busses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-busses',
+  templateUrl: './search-busses.component.html',
+  styleUrls: ['./search-busses.component.scss']
+})
+export class SearchBussesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
